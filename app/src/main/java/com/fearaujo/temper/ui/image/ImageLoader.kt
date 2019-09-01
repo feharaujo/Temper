97f1b46b1bb0270b6a35689ee847c1b62cb55d01
Felipe Trova de Araujo <fetrova@gmail.com>
2019-09-01 19:10:33 +0200

    Temper logo on image loading error

--- app/src/main/java/com/fearaujo/temper/ui/image/ImageLoader.kt
+++ app/src/main/java/com/fearaujo/temper/ui/image/ImageLoader.kt
@@ -2,13 +2,15 @@ package com.fearaujo.temper.ui.image
 
 import android.widget.ImageView
 import com.bumptech.glide.RequestManager
+import com.fearaujo.temper.R
 
 class ImageLoader(private val requestManager: RequestManager) {
 
     fun loadImage(url: String, imageView: ImageView) {
         requestManager.load(url)
-                .centerCrop()
-                .into(imageView)
+            .error(R.drawable.temper_icon)
+            .centerCrop()
+            .into(imageView)
     }
 
 }
