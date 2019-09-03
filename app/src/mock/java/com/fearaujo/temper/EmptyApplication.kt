9687459e0736a02ec7c56c31f88e3578e5236a8b
Felipe Trova de Araujo <fetrova@gmail.com>
2019-09-03 14:04:46 +0200

    Mock flavor setup

---
+++ app/src/mock/java/com/fearaujo/temper/EmptyApplication.kt
@@ -0,0 +1,13 @@
+package com.fearaujo.temper
+
+import android.app.Application
+import android.util.Log
+import com.example.mockwebserver.BaseMockWebServerTest
+
+class EmptyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
