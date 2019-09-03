72702acf9ddf507d4df959a6151dae0df93e3adf
Felipe Trova de Araujo <fetrova@gmail.com>
2019-09-03 14:05:47 +0200

    Idling resources to wait for request response

--- data/src/main/java/com/fearaujo/data/repository/RepositoryDataSource.kt
+++ data/src/main/java/com/fearaujo/data/repository/RepositoryDataSource.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.paging.PageKeyedDataSource
 import com.fearaujo.data.di.REPOSITORY_TAG
 import com.fearaujo.data.repository.remote.RemoteRepository
+import com.fearaujo.data.util.EspressoIdlingResource
 import com.fearaujo.model.Contractor
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
@@ -16,6 +17,10 @@ class RepositoryDataSource(
         private val remoteRepository: RemoteRepository,
         private val scope: CoroutineScope
 ) : PageKeyedDataSource<Int, Contractor>() {
+
+    init {
+        EspressoIdlingResource.increment()
+    }
 
     private companion object {
         const val INITIAL_PAGE = 0
@@ -28,6 +33,8 @@ class RepositoryDataSource(
             params: LoadInitialParams<Int>,
             callback: LoadInitialCallback<Int, Contractor>
     ) {
+        EspressoIdlingResource.decrement()
+
         fetchContractors(INITIAL_PAGE) { contractors ->
             callback.onResult(contractors, null, increasePage(INITIAL_PAGE))
         }
@@ -46,12 +53,16 @@ class RepositoryDataSource(
      * Remote call
      */
     private fun fetchContractors(page: Int, callback: (repositories: List<Contractor>) -> Unit) {
+        EspressoIdlingResource.increment()
+
         repoLiveData.postValue(RepositoryState.Loading)
 
         scope.launch(getErrorHandler() + supervisorJob) {
             val contractors = remoteRepository.fetchContractorsByDate(page)
             repoLiveData.postValue(RepositoryState.Success)
             callback.invoke(contractors)
+
+            EspressoIdlingResource.decrement()
         }
     }
 
@@ -59,6 +70,8 @@ class RepositoryDataSource(
         Log.e(REPOSITORY_TAG, "Repository error: $e")
         e.printStackTrace()
         repoLiveData.postValue(RepositoryState.OnError)
+
+        EspressoIdlingResource.decrement()
     }
 
     private fun increasePage(page: Int) = page.inc() + 1
